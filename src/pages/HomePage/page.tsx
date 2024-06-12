285dbefa4829619326a81cb8d7bb0492ec549b98
lopesdea91 <lopesdea91@gmail.com>
2024-06-12 08:43:47 -0300

    add shadcn

--- src/pages/HomePage/page.tsx
+++ src/pages/HomePage/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from 'components/ui/button'
 
 export default function HomePage() {
   return (
@@ -6,6 +7,7 @@ export default function HomePage() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <Button>button</Button>
     </div>
   )
 }
